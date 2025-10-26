626caab3af28567c1a2edd67d37725f673c3ffe8
Hmurdid <bash.kos.2007@gmail.com>
2025-10-26 13:43:03 +0100

    Delete tester.py

--- API/tester.py
+++
@@ -1,9 +0,0 @@
-from API.CustomRequest import SteamRequest
-
-info = SteamRequest('E42B53F855437905C3D239ED4AFB5BC2', '76561198836194029')
-print(info)
-
-response_player, response_games_data = info
-print(response_player.json())
-print(f"\n\n\n")
-print(response_games_data.json())
